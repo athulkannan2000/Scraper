dbd14c98ac8d222128a875e66c2fca9f90c2f9cc
Alfas Hakeem <alfashakeem@gmail.com>
2022-09-05 18:48:40 +0530

    updated db_update file and added new reqired field in all tables except scraped_data table

--- arabic_scrapper/db_update.py
+++ arabic_scrapper/db_update.py
@@ -1,8 +1,8 @@
 import os
-import re
 import pandas as pd
 import mysql.connector 
 from dotenv import load_dotenv
+from slugify import slugify
 
 load_dotenv()
 
@@ -19,25 +19,55 @@ cur = conn.cursor()
 cur.execute("""create table IF NOT EXISTS main_cat(
             id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
             main_category_name_en varchar(255),
-            main_category_name_ar text
+            main_category_name_ar text,
+            main_category_identifier varchar(255),
+            status int DEFAULT 1,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             );
             """)
+
 cur.execute("""create table IF NOT EXISTS sub_cat(
             id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
             sub_category_name_en varchar(255),
-            sub_category_name_ar text
+            sub_category_name_ar text,
+            sub_category_identifier varchar(255),
+            status int DEFAULT 1,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             );
             """)
+
 cur.execute("""create table IF NOT EXISTS category(
             id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
             category_name_en varchar(255),
-            category_name_ar text
+            category_name_ar text,
+            category_identifier varchar(255),
+            status int DEFAULT 1,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             );
             """)
+
 cur.execute("""create table IF NOT EXISTS agency(
             id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            agency_category_name_en varchar(255),
-            agency_category_name_ar text
+            agency_name_en varchar(255),
+            agency_name_ar text,
+            agency_identifier varchar(255),
+            status int DEFAULT 1,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            );
+            """)
+
+cur.execute("""create table IF NOT EXISTS platform(
+            id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            platform_name_en varchar(255),
+            platform_name_ar text,
+            platform_identifier varchar(255),
+            status int DEFAULT 1,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             );
             """)
 
@@ -61,69 +91,58 @@ cat_ar = df["Category - AR"].replace(to_replace= ['\r','\n'], value= '', regex=T
 agency_en = df["News Agency in English"].replace(to_replace= ['\r','\n'], value= '', regex=True).unique().tolist()
 agency_ar = df["News Agency in arabic"].replace(to_replace= ['\r','\n'], value= '', regex=True).unique().tolist()
 
-def id_helper(cat,keyval,removed):
-        fin="_".join([i[:keyval] for i in removed])
-        exist= identifiers.values()
-        if fin not in exist:
-            return "_".join([i[:keyval] for i in removed])
-        else:
-            id_helper(cat,keyval+1,removed)  
+#platform
+platform_en = df["Platform -EN"].replace(to_replace= ['\r','\n'], value= '', regex=True).unique().tolist()
+platform_ar = df["Platform - AR"].replace(to_replace= ['\r','\n'], value= '', regex=True).unique().tolist()
+   
 
-def identifier_generator(col,keyval):
-        exp="[A-Za-z0-9]+"
-        # print("from function",col)
-        for cat in col:
-            removed = re.findall(exp,cat) #finding only words
-            fin="_".join([i[:keyval] for i in removed])
-            exist= identifiers.values()
-            if fin not in exist:
-                fin="_".join([i[:keyval] for i in removed])
-                identifiers[cat]=fin
-            else:
-                identifiers[cat] = id_helper(cat,keyval+1,removed)
-        return identifiers      
-
-
-
- ############### encoding the values and inserting in reference table correspondingly ###################################
-identifiers={}
-main_cat=identifier_generator(main_cat_en,3)
-for en,ar,identifier in zip(main_cat_en,main_cat_ar,main_cat.values()):
-    select_stmt = "INSERT INTO main_cat (main_category_name_en,main_category_name_ar) SELECT %(category_name_en)s, %(category_name_ar)s FROM dual WHERE NOT EXISTS (SELECT * FROM main_cat WHERE main_category_name_en = %(category_name_en)s)"
+ ################################ inserting in reference table correspondingly ###################################
+for en,ar in zip(main_cat_en,main_cat_ar):
+    select_stmt = "INSERT INTO main_cat (main_category_name_en,main_category_name_ar,main_category_identifier) SELECT %(category_name_en)s, %(category_name_ar)s,%(category_identifier)s FROM dual WHERE NOT EXISTS (SELECT * FROM main_cat WHERE main_category_name_en = %(category_name_en)s)"
     cur.execute(select_stmt,{
         "category_name_en": en,
-        "category_name_ar": ar
+        "category_name_ar": ar,
+        "category_identifier": slugify(en)
     })
     conn.commit()
 
 
-identifiers={} 
-sub_cat=identifier_generator(sub_cat_en,3)
-for en,ar,identifier in zip(sub_cat_en,sub_cat_ar,sub_cat.values()):
-    select_stmt = "INSERT INTO sub_cat (sub_category_name_en,sub_category_name_ar) SELECT  %(category_name_en)s, %(category_name_ar)s FROM dual WHERE NOT EXISTS (SELECT * FROM sub_cat WHERE sub_category_name_en = %(category_name_en)s)"
+for en,ar in zip(sub_cat_en,sub_cat_ar):
+    select_stmt = "INSERT INTO sub_cat (sub_category_name_en,sub_category_name_ar,sub_category_identifier) SELECT  %(category_name_en)s, %(category_name_ar)s, %(category_identifier)s FROM dual WHERE NOT EXISTS (SELECT * FROM sub_cat WHERE sub_category_name_en = %(category_name_en)s)"
     cur.execute(select_stmt,{
         "category_name_en": en,
-        "category_name_ar": ar
+        "category_name_ar": ar,
+        "category_identifier": slugify(en)
     })
     conn.commit()
 
-identifiers={}
-category=identifier_generator(cat_en,3)
-for en,ar,identifier in zip(cat_en,cat_ar,category.values()):
-    select_stmt = "INSERT INTO category (category_name_en,category_name_ar) SELECT  %(category_name_en)s, %(category_name_ar)s FROM dual WHERE NOT EXISTS (SELECT * FROM category WHERE category_name_en = %(category_name_en)s)"
+
+for en,ar in zip(cat_en,cat_ar):
+    select_stmt = "INSERT INTO category (category_name_en,category_name_ar,category_identifier) SELECT  %(category_name_en)s, %(category_name_ar)s, %(category_identifier)s FROM dual WHERE NOT EXISTS (SELECT * FROM category WHERE category_name_en = %(category_name_en)s)"
     cur.execute(select_stmt,{
         "category_name_en": en,
-        "category_name_ar": ar
+        "category_name_ar": ar,
+        "category_identifier": slugify(en)
     })
     conn.commit()
 
-identifiers={}
-agency=identifier_generator(agency_en,3)
-for en,ar,identifier in zip(agency_en,agency_ar,agency.values()):
-    select_stmt = "INSERT INTO agency (agency_category_name_en,agency_category_name_ar) SELECT  %(category_name_en)s, %(category_name_ar)s FROM dual WHERE NOT EXISTS (SELECT * FROM agency WHERE agency_category_name_en = %(category_name_en)s)"
+
+for en,ar in zip(agency_en,agency_ar):
+    select_stmt = "INSERT INTO agency(agency_name_en,agency_name_ar,agency_identifier) SELECT  %(category_name_en)s, %(category_name_ar)s, %(category_identifier)s FROM dual WHERE NOT EXISTS (SELECT * FROM agency WHERE agency_name_en = %(category_name_en)s)"
     cur.execute(select_stmt,{
         "category_name_en": en,
-        "category_name_ar": ar
+        "category_name_ar": ar,
+        "category_identifier": slugify(en)
+    })
+    conn.commit()
+
+
+for en,ar in zip(platform_en,platform_ar):
+    select_stmt = "INSERT INTO platform (platform_name_en,platform_name_ar,platform_identifier) SELECT  %(category_name_en)s, %(category_name_ar)s, %(category_identifier)s FROM dual WHERE NOT EXISTS (SELECT * FROM platform WHERE platform_name_en = %(category_name_en)s)"
+    cur.execute(select_stmt,{
+        "category_name_en": en,
+        "category_name_ar": ar,
+        "category_identifier": slugify(en)
     })
     conn.commit()
 
