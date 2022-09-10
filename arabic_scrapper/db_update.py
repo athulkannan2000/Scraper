2eacdbea22d3888e87ab96550928168da5fd01a2
bharathwajan <rbw1906@gmail.com>
2022-09-10 17:51:55 +0530

    fix

--- arabic_scrapper/db_update.py
+++ arabic_scrapper/db_update.py
@@ -4,12 +4,12 @@ import mysql.connector
 from dotenv import load_dotenv
 from slugify import slugify
 
-#################### for verifying cron job####################
-# from datetime import datetime
-# now=datetime.now()
-# with open('/tmp/cron_log.txt',"a") as f:
-#     f.write("cornjob of db_update started at {} \n".format(now))
-##############################################################
+################### for verifying cron job####################
+from datetime import datetime
+now=datetime.now()
+with open('/tmp/cron_log.txt',"a") as f:
+    f.write("cornjob of db_update started at {} \n".format(now))
+#############################################################
 
 load_dotenv()
 
