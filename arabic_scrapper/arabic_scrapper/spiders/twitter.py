6f6dd40a256246415bc4f753aef1665e7ae131d8
bharathwajan <rbw1906@gmail.com>
2022-09-23 01:52:44 +0530

    bug_fixed in youtube and twitter

--- arabic_scrapper/arabic_scrapper/spiders/twitter.py
+++ arabic_scrapper/arabic_scrapper/spiders/twitter.py
@@ -1,4 +1,7 @@
 from ast import YieldFrom
+from multiprocessing import dummy
+from urllib import response
+from urllib.request import Request
 import scrapy
 import pandas as pd
 import tweepy
@@ -40,6 +43,7 @@ now = datetime.now()
 class TwitterSpider(scrapy.Spider):
     name = 'twitter'
     def start_requests(self):
+        pipeline=ArabicScrapperPipeline()
         for name,page_url,cat,main_cat,sub_cat,plat,media_typ,urgenc in zip(names,site_list,catagory,main_category,sub_category,platform,media_type,urgency):
             #print(page_url,type(page_url))
             username=page_url.split("/")[-1]
@@ -50,66 +54,126 @@ class TwitterSpider(scrapy.Spider):
             tweets = api.user_timeline(id=username, count=10)
             #print("$$$$$$$$$$$$$$$$$$$$$$NO of tweets$$$$$$$$$$$$$$$$$$",len(tweets))
             for tweet in tweets:
-                yield scrapy.Request(url="https://google.com",callback=self.details_saver,dont_filter=True,meta={"tweet":tweet,"name":name,"page_url":page_url,"cat":cat,"main_cat":main_cat,"sub_cat":sub_cat,"plat":plat,"media_typ":media_typ,"urgency":urgenc})
+                ####################
+                created=str(parser.parse(str(tweet.created_at)))
+                id="https://twitter.com/twitter/statuses/"+str(tweet.id)
+
+                try:
+                    tw_text=tweet.text
+                except:
+                    tw_text=tweet.full_text
+
+                media_type_="text"
+        
+                try:
+                    type=tweet.extended_entities["media"][0]["type"] #added try beacuse in some response extended entities is not there it is present only if the tweet contains image or video
+                except:
+                    type="text"
+                    pass
                 
+                if type=="video":
+                    media_type_="media"
+
+                try:
+                    video_url=tweet.extended_entities["media"][0]["video_info"]["variants"][0]["url"]
+                except :
+                    video_url=None
+
+                try:
+                    image_url=tweet.extended_entities["media"][0]["media_url"]
+                except:
+                    image_url=None
+                ####################
+                print("########i am here ")
+                pipeline=ArabicScrapperPipeline()
+                pipeline.process_item(item={
+                    "news_agency_name":name,
+                    "page_url": page_url,
+                    "category": cat,
+                    "title":str(tw_text),     
+                    "contents":None,
+                    "image_url":image_url,
+                    "date":str(now.strftime("%Y:%m:%d %H:%M:%S")),
+                    "author_name":None,
+                    "main_category":main_cat,
+                    "sub_category":sub_cat,
+                    "platform":plat,    
+                    "media_type":media_type_,
+                    "urgency":urgenc,
+                    "created_at":str(now.strftime("%Y:%m:%d %H:%M:%S")),
+                    "updated_at":str(now.strftime("%Y:%m:%d %H:%M:%S")),
+                    "deleted_at":None,
+                    "tweet_created_at":str(created),
+                    "tweet_text":str(tw_text),
+                    "tweet_id":str(id),
+                    "vdo_title":None,
+                    "vdo_description":None,
+                    "vdo_published_at":None,
+                    "vdo_thumbnail":None,
+                    "vdo_url":video_url
+                },spider="twitter")
+                yield scrapy.Request(url="https://google.com",callback=self.dummy)
+                # yield scrapy.Request(url="https://google.com",callback=self.details_saver,dont_filter=True,meta={"tweet":tweet,"name":name,"page_url":page_url,"cat":cat,"main_cat":main_cat,"sub_cat":sub_cat,"plat":plat,"media_typ":media_typ,"urgency":urgenc})
                 
-                
-    def details_saver(self,response):
-        # created=str(parser.parse(str(tweet.created_at)))
-        created=str(parser.parse(str(response.meta["tweet"].created_at)))
-        id="https://twitter.com/twitter/statuses/"+str(response.meta["tweet"].id)
-        try:
-            tw_text=response.meta["tweet"].text
-        except:
-            tw_text=response.meta["tweet"].full_text
-        media_type="text"
+    def dummy(self,response):
+        return None
 
-        try:
-            type=response.meta["tweet"].extended_entities["media"][0]["type"] #added try beacuse in some response extended entities is not there it is present only if the tweet contains image or video
-        except:
-            type="text"
-            pass
+    # def details_saver(self,response):
+    #     # created=str(parser.parse(str(tweet.created_at)))
+    #     created=str(parser.parse(str(response.meta["tweet"].created_at)))
+    #     id="https://twitter.com/twitter/statuses/"+str(response.meta["tweet"].id)
+    #     try:
+    #         tw_text=response.meta["tweet"].text
+    #     except:
+    #         tw_text=response.meta["tweet"].full_text
+    #     media_type="text"
 
-        if type=="video":
-            media_type="media"
+    #     try:
+    #         type=response.meta["tweet"].extended_entities["media"][0]["type"] #added try beacuse in some response extended entities is not there it is present only if the tweet contains image or video
+    #     except:
+    #         type="text"
+    #         pass
 
-        try:
-            video_url=response.meta["tweet"].extended_entities["media"][0]["video_info"]["variants"][0]["url"]
-        except :
-            video_url=None
+    #     if type=="video":
+    #         media_type="media"
 
-        try:
-            image_url=response.meta["tweet"].extended_entities["media"][0]["media_url"]
-        except:
-            image_url=None
+    #     try:
+    #         video_url=response.meta["tweet"].extended_entities["media"][0]["video_info"]["variants"][0]["url"]
+    #     except :
+    #         video_url=None
 
-        # #print("$$$$$$$$$$$$$$",type(id),type(tw_text),type(page_url),"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-        db_item=GeneralItem()
-        db_item["news_agency_name"]= response.meta["name"]
-        db_item["page_url"]= response.meta["page_url"]
-        db_item["category"]= response.meta["cat"]
-        db_item["title"]=str(tw_text)     
-        db_item["contents"]=None
-        db_item["image_url"]=image_url
-        db_item["date"]=str(now.strftime("%Y:%m:%d %H:%M:%S"))
-        db_item["author_name"]=None
-        db_item["main_category"]=str(response.meta["main_cat"])
-        db_item["sub_category"]=str(response.meta["sub_cat"])
-        db_item["platform"]=str(response.meta["plat"])
-        db_item["media_type"]=media_type
-        db_item["urgency"]=str(response.meta["urgency"])
-        db_item["created_at"]=str(now.strftime("%Y:%m:%d %H:%M:%S"))
-        db_item["updated_at"]=str(now.strftime("%Y:%m:%d %H:%M:%S"))
-        db_item["deleted_at"]=None
-        db_item["tweet_created_at"]=str(created) 
-        db_item["tweet_text"]=str(tw_text)
-        db_item["tweet_id"]=str(id)
+    #     try:
+    #         image_url=response.meta["tweet"].extended_entities["media"][0]["media_url"]
+    #     except:
+    #         image_url=None
 
-        db_item["vdo_title"]=None
-        db_item["vdo_description"]=None
-        db_item["vdo_published_at"]=None
-        db_item["vdo_thumbnail"]=None
-        db_item["vdo_url"]=video_url
-        #print("$$$$$$$$$$$$$$$$$$$$$ OOOOOKKKKKK saved sucessfully $$$$$$$$$$$$$$$$$$$$$$$:\n",db_item)
-        yield db_item
+    #     # #print("$$$$$$$$$$$$$$",type(id),type(tw_text),type(page_url),"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+    #     db_item=GeneralItem()
+    #     db_item["news_agency_name"]= response.meta["name"]
+    #     db_item["page_url"]= response.meta["page_url"]
+    #     db_item["category"]= response.meta["cat"]
+    #     db_item["title"]=str(tw_text)     
+    #     db_item["contents"]=None
+    #     db_item["image_url"]=image_url
+    #     db_item["date"]=str(now.strftime("%Y:%m:%d %H:%M:%S"))
+    #     db_item["author_name"]=None
+    #     db_item["main_category"]=str(response.meta["main_cat"])
+    #     db_item["sub_category"]=str(response.meta["sub_cat"])
+    #     db_item["platform"]=str(response.meta["plat"])
+    #     db_item["media_type"]=media_type
+    #     db_item["urgency"]=str(response.meta["urgency"])
+    #     db_item["created_at"]=str(now.strftime("%Y:%m:%d %H:%M:%S"))
+    #     db_item["updated_at"]=str(now.strftime("%Y:%m:%d %H:%M:%S"))
+    #     db_item["deleted_at"]=None
+    #     db_item["tweet_created_at"]=str(created) 
+    #     db_item["tweet_text"]=str(tw_text)
+    #     db_item["tweet_id"]=str(id)
+
+    #     db_item["vdo_title"]=None
+    #     db_item["vdo_description"]=None
+    #     db_item["vdo_published_at"]=None
+    #     db_item["vdo_thumbnail"]=None
+    #     db_item["vdo_url"]=video_url
+    #     #print("$$$$$$$$$$$$$$$$$$$$$ OOOOOKKKKKK saved sucessfully $$$$$$$$$$$$$$$$$$$$$$$:\n",db_item)
+    #     yield db_item
             # ea
