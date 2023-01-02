4cdaa9cb789ad8072797663a27189be7fa95d590
athulkannan2000 <athulkannan0123@gmail.com>
2023-01-02 10:11:07 +0530

    central_agency_for_public_tenders.py updation on class name

--- arabic_scrapper/arabic_scrapper/spiders/central_agency_for_public_tenders.py
+++ arabic_scrapper/arabic_scrapper/spiders/central_agency_for_public_tenders.py
@@ -10,7 +10,7 @@ from lxml import etree
 site_list,catagory,main_category,sub_category,platform,media_type,urgency = load_dataset_lists("central agency for public tenders")
 now = datetime.now()
 
-class YouthPublicAuthoritySpider(scrapy.Spider):
+class CentralAgencyForpublicTendersSpider(scrapy.Spider):
     name = 'capt'
     def start_requests(self):
         for page,catagori,main_categor,sub_categor,platfor,media_typ,urgenc in zip(site_list,catagory,main_category,sub_category,platform,media_type,urgency): 
